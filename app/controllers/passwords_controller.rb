a45e1d8bdb65d5423199637e27d94513121a4c90
DennisLu <dennislu@doubleservice.com>
2016-04-25 16:43:59 +0800

    fix flash.now bug

--- app/controllers/passwords_controller.rb
+++ app/controllers/passwords_controller.rb
@@ -1,7 +1,7 @@
 class PasswordsController < Devise::PasswordsController
 	def create
 		super do |resource|
-			flash[:alert] = t('activerecord.attributes.user.email') + t('errors.messages.invalid') if resource.email.nil?
+			flash.now[:alert] = t('activerecord.attributes.user.email') + t('errors.messages.invalid') if resource.email.nil?
 		end
 	end
 end
